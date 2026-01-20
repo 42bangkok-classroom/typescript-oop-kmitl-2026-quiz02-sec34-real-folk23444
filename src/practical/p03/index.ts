2ba8fb38c450ab11e77e0162f02c247b8841f1fe
folk23444 <Thanyaton.jv@gmail.com>
2026-01-21 00:07:25 +0700

    test

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,1 +1,21 @@
-export function filterUserById(id) {}
+import axios from "axios";
+
+interface getfilter {
+id: string;
+  name: string;
+  phone: string
+  address: string;
+}
+
+export async function filterUserById(id: getfilter | null) {
+try {
+const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+const foundUser = user.find((user) => user.id === id);
+return {
+  id: foundUser.id,
+  name: foundUser.name ?? null,
+  phone: foundUser.phone ?? null,
+  address: foundUser.address ?? null,
+};
+};
+}
