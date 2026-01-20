122d4edd5308043908695dbca502cd284e5a82b3
folk23444 <Thanyaton.jv@gmail.com>
2026-01-21 00:32:11 +0700

    test

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,21 +1,40 @@
 import axios from "axios";
 
-interface getfilter {
+type UserResponse = {
   id: number;
   name: string;
-  phone: string
-  address: string;
-}
+  phone: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+};
 
-export async function filterUserById(id: getfilter | null) {
+export async function filterUserById(id: number):
+Promise<UserResponse[]>
+{
 try {
 const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-const foundUser = user.find((user) => user.id === id);
+const foundUser = response.find((user) => user.id === id);
+
+if (!foundUser){
+return "Invalid id";
+}
+
 return {
   id: foundUser.id,
   name: foundUser.name ?? null,
   phone: foundUser.phone ?? null,
   address: foundUser.address ?? null,
+};
+catch{
+return "Invalid id"
 };
 };
 }
